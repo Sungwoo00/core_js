7ab79eaa38eff411a0a71cf4359ef84d2165bc8c
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-10-30 10:10:25 +0900

    variable

--- client/chapter/core/02.variable.js
+++ client/chapter/core/02.variable.js
@@ -4,27 +4,44 @@
 
 /* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
 
-// - 갯수 별 상품 가격 계산하기
-// - 구매 제품 가격의 총 합
-// - 1년 기준 일(day)자 수
-// - 구매 결제 여부
-// - 구매 결제 내역
-// - 브랜드 접두사
-// - 오늘의 운세
-// - 상품 정보
-
 /* variables ----------------------------------------------------------- */
+// - 갯수 별 상품 가격 계산하기
+let calcproductPriceQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 구매 결제 여부
+let isPayment = true;
+
+// - 구매 결제 내역
+let paymentList;
+
+// - 오늘의 운세
+let fortuneToday;
 
 /* constant variables -------------------------------------------------- */
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'Stussy';
+
+// - 상품 정보
+const PRODUCT_INFO = '...';
+
+//////////////////////
+
+const BRNAD = 'GOOGLE';
 
 let admin;
 let name;
 
-name = "john";
+name = 'john';
 admin = name;
 
 alert(admin);
 
-const PLANET = "EARTH";
+const PLANET = 'EARTH';
 
-let currentUserName = "me";
+let currentUserName = 'me';
