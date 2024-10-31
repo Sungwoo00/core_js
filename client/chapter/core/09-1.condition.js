0163b48fda88427fdb58ef479c43028e92e14e13
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-10-31 17:07:53 +0900

    condition

---
+++ client/chapter/core/09-1.condition.js
@@ -0,0 +1,62 @@
+/* ---------------- */
+/* Condition        */
+/* ---------------- */
+
+// const what = prompt(`자바스크립트의 '공식' 이름은 무엇일까요?`, '');
+// if ('ECMAScript' == what) {
+//   alert('정답입니다!');
+// } else {
+//   alert('모르셨나요? 정답은 ECMAScript 입니다!');
+// }
+
+// const input = +prompt('숫자 입력해주세요', ' ');
+
+// if (typeof input !== 'number') {
+//   no;
+// }
+
+// if (input > 0) {
+//   console.log(1);
+// } else if (input < 0) {
+//   console.log(-1);
+// } else if (input == 0) {
+//   console.log(0);
+// }
+
+// let a, b;
+
+// let result = a + b < 4 ? '미만' : '이상';
+
+let login;
+
+let message =
+  login == '직원'
+    ? '안녕하세요'
+    : '임원'
+      ? '환영합니다'
+      : ''
+        ? '로그인이 필요합니다'
+        : '';
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie;
+
+// 영화 볼거니?
+let goingToWatchMovie;
+
+// if 문(statement)
+
+// else 절(clause)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
