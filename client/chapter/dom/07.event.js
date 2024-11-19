52799256c7fdf8bb0ffe20f8ca890f3607047270
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-11-19 14:07:34 +0900

    - manipulation - attr - style - css

--- client/chapter/dom/07.event.js
+++ client/chapter/dom/07.event.js
@@ -2,15 +2,9 @@
 /* Event Handling        */
 /* --------------------- */
 
-
 /* 이벤트 핸들링 3가지 방법 --------------------------------------------------- */
 
 // 1. HTML 속성 : onclick="handler()"
-
-function handler() {
-  console.log('안녕');
-}
-
 
 // 2. DOM 프로퍼티 : element.onclick = handler
 
@@ -21,66 +15,82 @@ const home = getNode('.home');
 // 3. 메서드 : element.addEventListener(event, handler[, phase])
 
 function handler(e) {
-  console.log(e.offsetX)
+  console.log(e.wheelDeltaY);
+
+  // console.log(e.offsetX,e.offsetY);
 }
 
-about.addEventListener('click',handler); 
+about.addEventListener('wheel', handler);
 
-// remove => 버튼 disable 해줄 때 같이 해주면 리소스 아낄 수 있음, add와 remove의 함수는 동일해야함.
-
-
-// const remove = bindeEvent(about,'click',handler);
-
-// console.log(remove)
+// const remove = bindEvent(about,'click',handler);
 // home.addEventListener('click',remove)
-
 
 const ground = getNode('.ground');
 const ball = getNode('.ball');
 
-function handleBall(e){
+function handleBall({ offsetX: x, offsetY: y }) {
+  // const { offsetX:x, offsetY:y } = e;
 
-  const { offsetX:x, offsetY:y } = e
+  // const x = e.offsetX
+  // const y = e.offsetY
 
-  // const x = e.offsetX;
-  // const y = e.offsetY;
   const w = ball.offsetWidth;
   const h = ball.offsetHeight;
 
-  ball.style.transform = `translate(${x - w / 2 }px, ${y - h / 2}px)`
+  ball.style.transform = `translate(${x - w / 2}px,${y - h / 2}px)`;
 }
 
-ground.addEventListener('click',handleBall)
+ground.addEventListener('click', handleBall);
 
-function handleMove({ offsetX:x, offsetY:y }){
+function handleMove({ offsetX: x, offsetY: y }) {
   // const w = ball.offsetWidth;
   // const h = ball.offsetHeight;
-  // ball.style.transform = `translate(${x - w / 2 }px, ${y - h / 2}px)`
+  // ball.style.transform = `translate(${x - (w / 2)}px,${y - (h / 2)}px)`
 
   const template = `
-    <div class='emotion' style="top:${y}px; left:${x}px">👍</div>
-  `
-  insertLast(ground,template)
+    <div class="emotion" style="top:${y}px; left:${x}px">🥰</div>
+  `;
+  insertLast(ground, template);
 }
 
-ground.addEventListener('mousemove',handleMove)
+// debounce
 
+// function handle(e){
+//   console.log(e);
 
-//debounce 
+// }
 
-const input = getNode('input');
+ground.addEventListener('mousemove', debounce(handleMove, 100));
 
-function handleInput(){
-  if(this.value === 'sungwoo@gmail.com'){
-    console.log('ok');
-  }
-  
+window.addEventListener('resize', () => {
+  console.log('값 생성중...');
+});
+
+function debounce(callback, limit = 500) {
+  let timeout;
+
+  return function (e) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      callback.call(this, e); // call은 this를 바꿔치기 하고, 실행까지 시켜줌
+    }, limit);
+  };
 }
 
-input.addEventListener('input',handleInput);
+function throttle(callback, limit = 500) {
+  let wait = false;
 
+  return function () {
+    if (!wait) {
+      callback();
+      wait = true;
+      setTimeout(() => (wait = false), limit);
+    }
+  };
+}
 
-
+// throttle
 
 /* 이벤트 추가/제거 --------------------------------------------------------- */
 
