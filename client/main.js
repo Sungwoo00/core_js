810f1309a5607b9067b33e9467e14db8ccd0d20b
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-11-20 09:37:03 +0900

    계산기(이벤트 위임)

--- client/main.js
+++ client/main.js
@@ -1,11 +1,65 @@
-const a = "hello";
+import {
+  getNode as $,
+  getNodes,
+  insertLast,
+  clearContents,
+} from './lib/dom/index.js';
 
-function sum() {
-  console.log(1 * 2);
+function phase1() {
+  const first = getNode('#firstNumber');
+  const second = getNode('#secondNumber');
+  const result = getNode('.result');
+
+  // function handleCalc() {
+  //   const firstValue = Number(first.value) || 0;
+  //   const secondValue = Number(second.value) || 0;
+  //   result.innerHTML = firstValue + secondValue;
+  // }
+
+  function handleInput() {
+    const firstValue = Number(first.value) || 0;
+    const secondValue = +second.value;
+    const total = firstValue + secondValue;
+
+    clearContents(result);
+
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+
+    clearContents(first);
+    clearContents(second);
+    result.textContent = '-';
+  }
+
+  first.addEventListener('input', handleInput);
+  second.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
 }
 
-// alert("경고창");
+function phase2() {
+  const calculator = $('.calculator');
+  const result = $('.result');
+  const clear = $('#clear');
+  const numberInputs = [...document.querySelectorAll('input:not(#clear)')];
 
-// confirm("지울래?");
+  function handleInput() {
+    const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
 
-// prompt("안녕하세요");
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+
+    numberInputs.forEach(clearContents);
+    result.textContent = '-';
+  }
+
+  calculator.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+phase2();
