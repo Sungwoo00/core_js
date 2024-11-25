64be285b8194201e13dabbe0f4ff846c35bd064c
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-11-25 13:25:38 +0900

    POST 통신

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import {
   sungwoo,
   changeColor,
   renderSpinner,
+  clearContents,
   renderUserCard,
   renderEmptyCard,
 } from './lib/index.js';
@@ -18,7 +19,8 @@ import {
 // // 1. user 데이터 fetch
 // // 2. fetch 데이터 유저의 이름만 콘솔에 출력
 
-const END_POINT = 'https://jsonplaceholder.typicode.com/users';
+// const END_POINT = 'https://jsonplaceholder.typicode.com/users';
+const END_POINT = 'http://localhost:3000/users';
 
 const userCardInner = getNode('.user-card-inner');
 
@@ -49,7 +51,7 @@ async function renderUserList() {
       x: -100,
       opacity: 0,
       stagger: {
-        amount: 1,
+        each: 0.1,
         from: 'start',
       },
     });
@@ -59,7 +61,6 @@ async function renderUserList() {
 }
 
 renderUserList();
-
 function handleDeleteCard(e) {
   const button = e.target.closest('button');
 
@@ -70,7 +71,75 @@ function handleDeleteCard(e) {
 
   sungwoo.delete(`${END_POINT}/${index}`).then(() => {
     alert('삭제가 완료됐습니다.');
+
+    clearContents(userCardInner);
+    renderUserList();
   });
 }
 
 userCardInner.addEventListener('click', handleDeleteCard);
+
+const createButton = getNode('.create');
+const cancelButton = getNode('.cancel');
+const doneButton = getNode('.done');
+
+// create 버튼을 선택한다.
+// 클릭 이벤트를 바인딩한다.
+// create에 open 클래스를 추가한다.
+
+function handleCreate() {
+  // this.classList.add('open');
+  gsap.to('.pop', { autoAlpha: 1 });
+}
+
+function handleCancel(e) {
+  e.stopPropagation();
+  // createButton.classList.remove('open');
+  gsap.to('.pop', { autoAlpha: 0 });
+}
+
+const form = getNode('.pop form');
+
+// async function handelDone(e) {
+//   e.preventDefault();
+
+//   const username = getNode('#nameField').value;
+//   const email = getNode('#emailField').value;
+//   const website = getNode('#siteField').value;
+
+//   const newUser = { username, email, website };
+
+//   try {
+//     await sungwoo.post(END_POINT, newUser);
+
+//     popup.classList.remove('open');
+//     form.reset();
+
+//     clearContents(userCardInner);
+//     renderUserList();
+//   } catch (error) {
+//     alert('유효한 정보를 입력해주세요');
+//   }
+// }
+
+function handleDone(e) {
+  e.preventDefault();
+
+  const username = getNode('#nameField').value;
+  const email = getNode('#emailField').value;
+  const website = getNode('#siteField').value;
+
+  tiger.post(END_POINT, { username, email, website }).then(() => {
+    gsap.to('.pop', { autoAlpha: 0 });
+    clearContents(userCardInner);
+    renderUserList();
+
+    getNode('#nameField').value = '';
+    getNode('#emailField').value = '';
+    getNode('#siteField').value = '';
+  });
+}
+
+createButton.addEventListener('click', handleCreate);
+cancelButton.addEventListener('click', handleCancel);
+doneButton.addEventListener('submit', handleDone);
