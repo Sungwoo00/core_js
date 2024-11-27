e1dd458cc491682be1d5ac289838b002d19962af
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-11-27 13:31:33 +0900

    web component

---
+++ client/components/Button/Button.js
@@ -0,0 +1,52 @@
+export class Button extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+
+    this.state = {
+      active: this.getAttribute('active') || false,
+    };
+
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ['active'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'active') {
+      this.state.active = newValue === 'true';
+      this.render();
+    }
+  }
+
+  handleClick() {
+    const newActiveState = !this.state.active;
+    this.setAttribute('active', newActiveState);
+  }
+
+  render() {
+    const { active } = this.state;
+
+    this.shadowRoot.innerHTML = `
+    <style>@import url('./components/Button/Button.css');
+    button {
+      background-color: ${active ? 'orange' : 'red'}
+    }
+    </style>
+    <button 
+      type="button",
+      aria-pressed="${active}",
+      aria-label="${active ? '활성화' : '비활성화'}"
+    >
+      ${active ? '👀' : '🔎'}
+    </button>
+  `;
+
+    this.shadowRoot
+      .querySelector('button')
+      .addEventListener('click', this.handleClick.bind(this));
+  }
+}
--- client/main.js
+++ client/main.js
@@ -1,30 +1,48 @@
-import { getNode, setStorage, getStorage, deleteStorage } from './lib/index.js';
+import { Button } from './components/Button/Button.js';
 
-// 1. 인풋 이벤트 바인딩
-// 2. 인풋 값을 로컬 스토리지에 저장(타이핑 하는 매 순간마다)
-// 3. init함수 안에서 로컬스토리지에 있는 값을 가져와 인풋의 value로 설정
-// 새로고침 => 데이터 유지
+const app = document.getElementById('app');
 
-const textField = getNode('#textField');
-const clearButton = getNode('button[data-name="clear"]');
-
-function handleInput() {
-  const value = this.value;
-  setStorage('text', value);
+function defineElements() {
+  customElements.define('custom-button', Button);
 }
 
-function handleClear() {
-  textField.value = '';
-  deleteStorage('text');
-}
+defineElements();
 
-function init() {
-  getStorage('text').then((res) => {
-    textField.value = res;
-  });
-}
+const buttonElement = document.createElement('custom-button');
 
-textField.addEventListener('input', handleInput);
-clearButton.addEventListener('click', handleClear);
+app.append(buttonElement);
+// // console.log(temp.content.cloneNode(true));
+// class MyElement extends HTMLElement {
+//   constructor() {
+//     super();
 
-init();
+//     this.attachShadow({ mode: 'open' });
+//     this._render();
+//     this.card = this.shadowRoot.querySelector('.card');
+//   }
+
+//   connectedCallback() {
+//     this.card.addEventListener('click', this.handleClick.bind(this));
+//   }
+
+//   handleClick() {
+//     console.log(this.getAttribute('data-name'));
+//   }
+
+//   _render() {
+//     this.shadowRoot.innerHTML = `
+
+//         <style>
+//         @import url('./style.css');
+//         </style>
+
+//         <div class="card">
+//           <slot name="title">Default Title</slot>
+//           <slot name="content">Default Contents</slot>
+//           <slot></slot>
+//         </div>
+//     `;
+//   }
+// }
+
+// customElements.define('my-element', MyElement);
