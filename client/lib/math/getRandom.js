2ae2d0babea530f2b56230b4860747ce0136e888
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-11-29 01:05:02 +0900

    모든 파일 삭제

--- client/lib/math/getRandom.js
+++
@@ -1,7 +0,0 @@
-export function getRandom(n) {
-  return Math.floor(Math.random() * n);
-}
-
-// function getRandomNumber(array) {
-//   return array[Math.floor(Math.random() * array.length)];
-// }
