cfb2a30a9cec941bf222fc285aa71ed9d21257cd
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-11-29 15:05:09 +0900

    lit, shop

---
+++ src/lit/setp04.js
@@ -0,0 +1,31 @@
+import { LitElement, html } from 'lit';
+
+class NameTag extends LitElement {
+  static properties = {
+    name: {},
+  };
+
+  constructor() {
+    super();
+    this.name = '너의 이름은?';
+  }
+
+  handleInput(e) {
+    this.name = e.target.value;
+  }
+
+  handleClick() {
+    alert(this.name);
+  }
+
+  render() {
+    return html` 
+    <p>안녕, ${this.name}</p> 
+    <input @input=${this.handleInput} type="text" placeholder='이름을 입력하세요'></input>
+    <button @click=${this.handleClick} type="button">click me</button>
+    `;
+  }
+}
+
+customElements.define('name-tag', NameTag);
+app.append(document.createElement('name-tag'));
