bf01630382975c216a2766957573982d348cc29e
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-12-06 15:19:42 +0900

    detail, newPost

--- src/components/ProductList.ts
+++ src/components/ProductList.ts
@@ -1,8 +1,9 @@
 import { LitElement, html, css, CSSResultGroup } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 import resetCSS from '../Layout/restCSS';
-import { Item, Product } from '../@types/type';
+import { Auth, Product } from '../@types/type';
 import { getPbImageURL } from '../api/getPbImageURL';
+import gsap from 'gsap';
 
 @customElement('product-list')
 class ProductList extends LitElement {
@@ -13,6 +14,8 @@ class ProductList extends LitElement {
     totalItems: 0,
     totalPages: 0,
   };
+
+  @state() loginData = {} as Auth;
 
   static styles: CSSResultGroup = [
     resetCSS,
@@ -30,41 +33,53 @@ class ProductList extends LitElement {
           grid-template-columns: repeat(2, minmax(0, 1fr));
           gap: 2rem;
           margin: 2.5rem;
-        }
 
-        & li {
-          & a {
-            max-width: 30vw;
-            display: flex;
-            flex-direction: column;
-            gap: 0.6rem;
+          & li {
+            & a {
+              max-width: 30vw;
+              display: flex;
+              flex-direction: column;
+              gap: 0.6rem;
+            }
+          }
+
+          .description {
+            font-size: 0.8rem;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          }
+
+          .price {
+            color: gray;
+            text-decoration: line-through;
+          }
+
+          .discount {
+            font-size: 1.2rem;
+            color: red;
+          }
+
+          .real-price {
+            font-weight: bold;
           }
         }
+      }
 
-        .decription {
-          font-size: 0.8rem;
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-        }
-
-        .price {
-          color: gray;
-          text-decoration: line-through;
-        }
-
-        .discount {
-          font-size: 1.2rem;
-          color: red;
-        }
-        .real-price {
-          font-weight: bold;
-        }
+      .new-post {
+        padding: 0.5rem 1rem;
+        background-color: dodgerblue;
+        color: white;
+        border-radius: 20px;
+        position: fixed;
+        transform: translateX(-50%);
+        left: 50%;
+        bottom: 2rem;
       }
     `,
   ];
 
-  connectedCallback(): void {
+  connectedCallback() {
     super.connectedCallback();
     this.fetchData();
   }
@@ -74,37 +89,52 @@ class ProductList extends LitElement {
 
     const data = await response.json();
     this.data = data;
+
+    this.loginData = JSON.parse(localStorage.getItem('auth') ?? '{}');
   }
 
-  createProductCard(item: Item) {
-    const tag = html`
-      <li>
-        <a href="/">
-          <figure>
-            <img src="${getPbImageURL(item)}" alt="" />
-          </figure>
-          <span class="brand">${item.brand}</span>
-          <span class="description">${item.description}</span>
-          <span class="price">${item.price}원</span>
-          <div>
-            <span class="discount">${item.discount}%</span>
-            <span class="real-price">${item.price - (item.price * item.discount) / 100}원</span>
-          </div>
-        </a>
-      </li>
-    `;
-    return tag;
+  updated(changedProperties: Map<string | number | symbol, unknown>): void {
+    super.updated(changedProperties);
+
+    const item = this.renderRoot.querySelectorAll('.product-item');
+
+    if (item.length > 0) {
+      gsap.from(item, {
+        y: 30,
+        opacity: 0,
+        stagger: 0.2,
+      });
+    }
   }
 
   render() {
-    const itemsMap = this.data.items.map((item) => this.createProductCard(item));
+    const { isAuth } = this.loginData;
 
     return html`
       <div class="container">
         <ul>
-          ${itemsMap}
+          ${this.data.items.map(
+            (item) => html`
+              <li class="product-item">
+                <a href="${isAuth ? `/src/pages/detail/index.html?product=${item.id}` : `/`}">
+                  <figure>
+                    <img src="${getPbImageURL(item)}" alt="" />
+                  </figure>
+                  <span class="brand">${item.brand}</span>
+                  <span class="description">${item.description}</span>
+                  <span class="price">${item.price.toLocaleString()}원</span>
+                  <div>
+                    <span class="discount">${item.discount}%</span>
+                    <span class="real-price">${(item.price - item.price * item.discount * 0.01).toLocaleString()}원</span>
+                  </div>
+                </a>
+              </li>
+            `
+          )}
         </ul>
       </div>
+
+      <a class="new-post" href="/src/pages/newPost/">+ 상품추가</a>
     `;
   }
 }
---
+++ src/pages/newPost/index.js
@@ -0,0 +1,3 @@
+import '@/Layout/Headers.ts';
+import '@/pages/newPost/newPost';
+import '@/components/NewPost';
